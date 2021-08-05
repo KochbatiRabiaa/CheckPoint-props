5bef786b8f5faa938deea2821fb1ec093c03a6d8
KochbatiRabiaa <85884134+KochbatiRabiaa@users.noreply.github.com>
2021-08-05 13:33:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Profile from "./profile/Profile"
 import Photo1 from "./profile/photo1.jpg"
 
 
+const styleimg= { opacite:'2' ,
+                  width:'400PX',
+                  float:'center' ,
+                  border:'2px solid #fff',
+                  mozBoxShadow: '10px 10px 5px #ccc',
+                  webkitBoxShadow:' 10px 10px 5px #ccc',
+                  boxShadow: '10px 10px 5px #ccc',
+                  mozBorderRadius:'25px',
+                  webkitBorderRadius:'25px',
+                  borderRadius:'25px' }
+
+
 function App() {
-  return (
+ return (
     <div className="App">
       <Profile fullName="Kochbati Rabiaa"
-        bio="A master degree in analytical chemistry from the University of Tunis: Faculty of Sciences of Tunis.
-      A wife and a mother of a beautiful twin"
-       profession=" chemistry teacher"
-       children={Photo1} / >
-      
+        bio="A master degree in analytical chemistry from the University of Tunis: Faculty of Sciences of Tunis.A wife and a mother of a beautiful twin"
+       profession=" chemistry teacher">
+          <img  style={styleimg}  src={Photo1} alt=""/> 
+   </Profile>
     </div>
   );
 }
-
 export default App;
